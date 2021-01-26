322388e573ade0723319a93cfe1a13259b5e20a2
crslegend <risheng17@gmail.com>
2021-01-26 21:50:35 +0800

    refactor sidebar codes for reusability

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Avatar, Drawer, List, ListItem, Typography } from "@material-ui/core";
-import { NavLink } from "react-router-dom";
-import { Dashboard } from "@material-ui/icons";
+import { Drawer, List } from "@material-ui/core";
 
 const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
@@ -24,41 +22,9 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     flexDirection: "column",
   },
-  logo: {
-    padding: "30px 50px",
-    maxWidth: "100%",
-  },
-  listItem: {
-    width: "100%",
-    padding: 10,
-    borderLeft: "5px solid #fff",
-    "&:hover": {
-      backgroundColor: "#F4F4F4",
-      borderLeft: "5px solid #F4F4F4",
-    },
-  },
-  listIcon: {
-    marginLeft: "15px",
-    marginRight: "20px",
-  },
-  activeLink: {
-    width: "100%",
-    padding: 10,
-    color: theme.palette.primary.main,
-    backgroundColor: "#F4F4F4",
-    borderLeft: "5px solid",
-    "&:hover": {
-      borderLeft: "5px solid #437FC7",
-    },
-  },
-  avatar: {
-    width: theme.spacing(15),
-    height: theme.spacing(15),
-    fontSize: "60px",
-  },
 }));
 
-const Sidebar = () => {
+const Sidebar = ({ head, list }) => {
   const classes = useStyles();
 
   return (
@@ -78,35 +44,9 @@ const Sidebar = () => {
             alignItems: "center",
           }}
         >
-          <div style={{ marginTop: "30px", marginBottom: "10px" }}>
-            <Avatar className={classes.avatar}>P</Avatar>
-          </div>
-          <Typography variant="h6">Name here</Typography>
-          <Typography variant="body1">Position here</Typography>
+          {head}
         </div>
-
-        <List style={{ marginTop: "20px" }}>
-          <ListItem
-            component={NavLink}
-            to="/member"
-            activeClassName={classes.activeLink}
-            className={classes.listItem}
-            button
-          >
-            <Dashboard className={classes.listIcon} />
-            <Typography variant="h5">Dashboard</Typography>
-          </ListItem>
-          <ListItem
-            component={NavLink}
-            to="/industry"
-            activeClassName={classes.activeLink}
-            className={classes.listItem}
-            button
-          >
-            <Dashboard className={classes.listIcon} />
-            <Typography variant="h5">Dashboard</Typography>
-          </ListItem>
-        </List>
+        <List style={{ marginTop: "20px" }}>{list}</List>
       </div>
     </Drawer>
   );
--- src/member/MemberLanding.jsx
+++ src/member/MemberLanding.jsx
@@ -1,9 +1,10 @@
 import React, { Fragment } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Navbar from "../components/Navbar";
-import { Link } from "react-router-dom";
-import { Button, ListItem, Typography } from "@material-ui/core";
+import { Link, NavLink } from "react-router-dom";
+import { Avatar, Button, ListItem, Typography } from "@material-ui/core";
 import Sidebar from "../components/Sidebar";
+import { Dashboard } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,6 +12,34 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     flexDirection: "column",
     fontDisplay: "swap",
+  },
+  listItem: {
+    width: "100%",
+    padding: 10,
+    borderLeft: "5px solid #fff",
+    "&:hover": {
+      backgroundColor: "#F4F4F4",
+      borderLeft: "5px solid #F4F4F4",
+    },
+  },
+  listIcon: {
+    marginLeft: "15px",
+    marginRight: "20px",
+  },
+  activeLink: {
+    width: "100%",
+    padding: 10,
+    color: theme.palette.primary.main,
+    backgroundColor: "#F4F4F4",
+    borderLeft: "5px solid",
+    "&:hover": {
+      borderLeft: "5px solid #437FC7",
+    },
+  },
+  avatar: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+    fontSize: "60px",
   },
 }));
 
@@ -60,10 +89,45 @@ const MemberLanding = () => {
     </Fragment>
   );
 
+  const sidebarHead = (
+    <Fragment>
+      <div style={{ marginTop: "30px", marginBottom: "10px" }}>
+        <Avatar className={classes.avatar}>P</Avatar>
+      </div>
+      <Typography variant="h6">Name here</Typography>
+      <Typography variant="body1">Position here</Typography>
+    </Fragment>
+  );
+
+  const sidebarList = (
+    <Fragment>
+      <ListItem
+        component={NavLink}
+        to="/member"
+        activeClassName={classes.activeLink}
+        className={classes.listItem}
+        button
+      >
+        <Dashboard className={classes.listIcon} />
+        <Typography variant="h5">Dashboard</Typography>
+      </ListItem>
+      <ListItem
+        component={NavLink}
+        to="/industry"
+        activeClassName={classes.activeLink}
+        className={classes.listItem}
+        button
+      >
+        <Dashboard className={classes.listIcon} />
+        <Typography variant="h5">Dashboard</Typography>
+      </ListItem>
+    </Fragment>
+  );
+
   return (
     <div className={classes.root}>
       <Navbar navbarItems={memberNavbar} />
-      <Sidebar />
+      <Sidebar head={sidebarHead} list={sidebarList} />
     </div>
   );
 };
