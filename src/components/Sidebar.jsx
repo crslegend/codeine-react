b3909e6e213ad39d3058f523615a856b58155b00
crslegend <risheng17@gmail.com>
2021-01-26 21:36:48 +0800

    added navlinks with css stylings

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Drawer } from "@material-ui/core";
+import { Avatar, Drawer, List, ListItem, Typography } from "@material-ui/core";
+import { NavLink } from "react-router-dom";
+import { Dashboard } from "@material-ui/icons";
 
 const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
-    height: "calc(100vh - 68px)",
+    height: "calc(100vh - 70px)",
     bottom: 0,
     position: "absolute",
   },
   drawerPaper: {
     width: drawerWidth,
-    height: "calc(100vh - 68px)",
+    height: "calc(100vh - 70px)",
     bottom: 0,
     position: "absolute",
-    backgroundColor: theme.palette.primary.main,
+    // backgroundColor: theme.palette.primary.main,
   },
   drawerContent: {
     display: "flex",
@@ -27,36 +29,32 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: "100%",
   },
   listItem: {
-    borderRadius: "10px",
-    margin: "5px 10px",
-    width: "calc(100% - 20px)",
-    color: "#333030",
+    width: "100%",
+    padding: 10,
+    borderLeft: "5px solid #fff",
     "&:hover": {
-      backgroundColor: theme.palette.primary.light,
+      backgroundColor: "#F4F4F4",
+      borderLeft: "5px solid #F4F4F4",
     },
   },
   listIcon: {
+    marginLeft: "15px",
     marginRight: "20px",
   },
   activeLink: {
-    borderRadius: "10px",
-    margin: "5px 10px",
-    width: "calc(100% - 20px)",
-    color: "#FFFFFF",
-    backgroundColor: theme.palette.primary.main,
+    width: "100%",
+    padding: 10,
+    color: theme.palette.primary.main,
+    backgroundColor: "#F4F4F4",
+    borderLeft: "5px solid",
     "&:hover": {
-      backgroundColor: theme.palette.primary.light,
+      borderLeft: "5px solid #437FC7",
     },
   },
-  logout: {
-    borderRadius: "10px",
-    margin: "30px 15px",
-    width: "calc(100% - 30px)",
-    color: "#FFF",
-    backgroundColor: theme.palette.error.main,
-    "&:hover": {
-      backgroundColor: theme.palette.error.light,
-    },
+  avatar: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+    fontSize: "60px",
   },
 }));
 
@@ -71,7 +69,46 @@ const Sidebar = () => {
         paper: classes.drawerPaper,
       }}
       anchor="left"
-    ></Drawer>
+    >
+      <div className={classes.drawerContent}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <div style={{ marginTop: "30px", marginBottom: "10px" }}>
+            <Avatar className={classes.avatar}>P</Avatar>
+          </div>
+          <Typography variant="h6">Name here</Typography>
+          <Typography variant="body1">Position here</Typography>
+        </div>
+
+        <List style={{ marginTop: "20px" }}>
+          <ListItem
+            component={NavLink}
+            to="/member"
+            activeClassName={classes.activeLink}
+            className={classes.listItem}
+            button
+          >
+            <Dashboard className={classes.listIcon} />
+            <Typography variant="h5">Dashboard</Typography>
+          </ListItem>
+          <ListItem
+            component={NavLink}
+            to="/industry"
+            activeClassName={classes.activeLink}
+            className={classes.listItem}
+            button
+          >
+            <Dashboard className={classes.listIcon} />
+            <Typography variant="h5">Dashboard</Typography>
+          </ListItem>
+        </List>
+      </div>
+    </Drawer>
   );
 };
 
