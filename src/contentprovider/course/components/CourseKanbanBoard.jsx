7858c37299db4c2469f529980248a1defa97b493
crslegend <risheng17@gmail.com>
2021-02-09 20:37:49 +0800

    added condes to persist reorder of tasks

--- src/contentprovider/course/components/CourseKanbanBoard.jsx
+++ src/contentprovider/course/components/CourseKanbanBoard.jsx
@@ -27,7 +27,40 @@ const CourseKanbanBoard = () => {
 
   const [state, setState] = useState(data);
 
-  const handleDragEnd = (result) => {};
+  const handleDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    };
+
+    const newState = {
+      ...state,
+      columns: {
+        ...state.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+
+    setState(newState);
+  };
 
   return (
     state &&
