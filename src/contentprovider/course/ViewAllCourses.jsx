9c7dbf4fd4a41c4cf7b10ef50750e674fa36b639
crslegend <risheng17@gmail.com>
2021-02-11 12:27:45 +0800

    added popover for more options for each course

--- src/contentprovider/course/ViewAllCourses.jsx
+++ src/contentprovider/course/ViewAllCourses.jsx
@@ -1,9 +1,19 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Button, Typography } from "@material-ui/core";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  IconButton,
+  Popover,
+} from "@material-ui/core";
 import PageTitle from "../../components/PageTitle";
-import { Add } from "@material-ui/icons";
+import { Add, MoreVert } from "@material-ui/icons";
 import { Link, useHistory } from "react-router-dom";
+
+import Service from "../../AxiosService";
 
 const useStyles = makeStyles((theme) => ({
   titleSection: {
@@ -20,11 +30,56 @@ const useStyles = makeStyles((theme) => ({
       color: "#000",
     },
   },
+  courses: {
+    marginTop: "30px",
+  },
+  card: {
+    width: 200,
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%",
+  },
+  popoverContents: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  popoverButtons: {
+    width: 150,
+    textTransform: "capitalize",
+  },
 }));
 
 const ViewAllCourses = () => {
   const classes = useStyles();
   // const history = useHistory();
+
+  const [allCourses, setAllCourses] = useState();
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const getAllCourses = () => {
+    Service.client
+      .get(`/courses/ebd49e8d-a724-432a-938b-5de1c0ccde9f`)
+      .then((res) => {
+        // console.log(res);
+        setAllCourses(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  useEffect(() => {
+    getAllCourses();
+  }, []);
+
+  console.log(allCourses);
 
   return (
     <Fragment>
@@ -39,6 +94,45 @@ const ViewAllCourses = () => {
         >
           Create New Course
         </Button>
+      </div>
+      <div className={classes.courses}>
+        <Card className={classes.card}>
+          <CardMedia
+            className={classes.media}
+            image={allCourses && allCourses.thumbnail}
+            title={allCourses && allCourses.title}
+          />
+          <CardContent>{allCourses && allCourses.title}</CardContent>
+          <CardActions style={{ float: "right" }}>
+            <IconButton onClick={handleClick} size="small">
+              <MoreVert />
+            </IconButton>
+            <Popover
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+              anchorReference="anchorPosition"
+              anchorPosition={{ top: 405, left: 475 }}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+            >
+              <div className={classes.popoverContents}>
+                <Button className={classes.popoverButtons}>
+                  Reply Comments
+                </Button>
+                <Button className={classes.popoverButtons}>Edit Course</Button>
+                <Button className={classes.popoverButtons}>
+                  <span style={{ color: "red" }}>Delete Course</span>
+                </Button>
+              </div>
+            </Popover>
+          </CardActions>
+        </Card>
       </div>
     </Fragment>
   );
