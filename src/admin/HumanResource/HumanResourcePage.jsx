7f81df074fb0a63ddf2b326a74abdf992864fea5
unknown <henchicky@gmail.com>
2021-02-23 15:37:21 +0800

    minor changes

--- src/admin/HumanResource/HumanResourcePage.jsx
+++ src/admin/HumanResource/HumanResourcePage.jsx
@@ -1,19 +1,22 @@
 import React, { Fragment, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
-import { Typography, Avatar } from "@material-ui/core";
+import {
+  Typography,
+  Avatar,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Button,
+  IconButton,
+  AppBar,
+  Tabs,
+  Tab,
+  Box,
+} from "@material-ui/core";
 import { DataGrid } from "@material-ui/data-grid";
-import AppBar from "@material-ui/core/AppBar";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import Box from "@material-ui/core/Box";
 import SearchBar from "material-ui-search-bar";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 import Service from "../../AxiosService";
 import Grid from "@material-ui/core/Grid";
