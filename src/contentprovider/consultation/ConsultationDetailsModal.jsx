850a0d0e1b486ee531ab46fb04d2062ee1590a70
Tze Ming <e0311042@u.nus.edu>
2021-02-27 20:35:11 +0800

    Add URL regex check

--- src/contentprovider/consultation/ConsultationDetailsModal.jsx
+++ src/contentprovider/consultation/ConsultationDetailsModal.jsx
@@ -271,6 +271,15 @@ const ConsultationDetailsModal = ({
       return;
     }
 
+    if (!/^https?:\/\/([\w\d-]+\.)+\w{2,}(\/.+)?$/.test(slot.meeting_link)) {
+      setSnackbar({
+        message: "Enter a valid URL for the meeting link",
+        severity: "error",
+      });
+      setSnackbarOpen(true);
+      return;
+    }
+
     if (timeError.err) {
       return;
     }
