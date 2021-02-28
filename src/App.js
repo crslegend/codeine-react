453c09a0ea2cfa0a5438bd063e9a8d057580257c
Hendrick <43085749+henchicky@users.noreply.github.com>
2021-02-28 08:34:32 +0800

    Revert "Hendrick"

--- src/App.js
+++ src/App.js
@@ -1,115 +1,25 @@
-import React from "react";
-// import "./App.css";
-import { Route, Switch } from "react-router-dom";
-import MemberLandingPage from "./member/landing/MemberLandingPage";
-import MemberRegisterPage from "./member/auth/MemberRegisterPage";
-import MemberLoginPage from "./member/auth/MemberLoginPage";
-import ContentProviderLanding from "./contentprovider/LandingPage/ContentProviderLanding";
-import AdminLoginPage from "./admin/auth/AdminLoginPage";
-import AdminRoutesPage from "./admin/AdminRoutesPage";
-import MemberHome from "./member/profile/MemberRoutesPage";
-import PrivateRoute from "./components/PrivateRoute.jsx";
-import NotFound from "./components/NotFound";
-import ContentProviderHome from "./contentprovider/ContentProviderRoutes";
-import ContentProviderLoginPage from "./contentprovider/auth/ContentProviderLoginPage";
-import ContentProviderRegisterPage from "./contentprovider/auth/ContentProviderRegisterPage";
-import LandingPageRoute from "./components/LandingPageRoute";
-import ViewAllCourses from "./member/course/ViewAllCourses";
-import ViewCourseDetails from "./member/course/ViewCourseDetails";
-import EnrollCourse from "./member/course/EnrollCourse";
-import BookConsult from "./member/course/BookConsult";
-import PaymentSuccess from "./components/PaymentSuccess";
-import ResetPassword from "./resetPassword/ResetPasswordPage";
-import NewPassword from "./resetPassword/NewPasswordPage";
-import Activation from "./resetPassword/ActivationPage";
-import AdminRoute from "./components/AdminRoute";
-import PartnerRoute from "./components/PartnerRoute";
-import MemberRoute from "./components/MemberRoute";
-import MemberAndPublicRoute from "./components/MemberAndPublicRoute";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <Switch>
-      <LandingPageRoute
-        exact
-        path="/partner"
-        render={() => <ContentProviderLanding />}
-        user="partner"
-      />
-      <PartnerRoute
-        path="/partner/home"
-        render={() => <ContentProviderHome />}
-        user="partner"
-      />
-      <LandingPageRoute
-        exact
-        path="/partner/login"
-        render={() => <ContentProviderLoginPage />}
-        user="partner"
-      />
-      <LandingPageRoute
-        exact
-        path="/partner/register"
-        render={() => <ContentProviderRegisterPage />}
-        user="partner"
-      />
-      <MemberAndPublicRoute exact path="/" component={MemberLandingPage} />
-      <MemberAndPublicRoute exact path="/courses" component={ViewAllCourses} />
-      <MemberAndPublicRoute
-        exact
-        path="/courses/:id"
-        strict
-        sensitive
-        component={ViewCourseDetails}
-      />
-      <MemberRoute
-        exact
-        path="/courses/enroll/:id"
-        strict
-        sensitive
-        component={EnrollCourse}
-      />
-      <MemberRoute
-        exact
-        path="/courses/enroll/consultation/:id"
-        strict
-        sensitive
-        component={BookConsult}
-      />
-      <PrivateRoute
-        path="/member/home"
-        render={() => <MemberHome />}
-        user="member"
-      />
-      <LandingPageRoute
-        exact
-        path="/member/login"
-        component={MemberLoginPage}
-        user="member"
-      />
-      <LandingPageRoute
-        exact
-        path="/member/register"
-        component={MemberRegisterPage}
-        user="member"
-      />
-      {/* <Route exact path="/industry" component={IndustryLanding} /> */}
-      <Route exact path="/admin/login" component={AdminLoginPage} />
-      <AdminRoute path="/admin" component={AdminRoutesPage} user="admin" />
-      <Route exact path="/admin/humanresource" component={AdminRoutesPage} />
-      <Route path="/payment/success" component={PaymentSuccess} />
-      <Route
-        exact
-        path="/resetPassword/:id"
-        strict
-        sensitive
-        component={ResetPassword}
-      />
-      <Route path="/reset-password" component={NewPassword} />
-      <Route exact path="/verify/:id" strict sensitive component={Activation} />
-      <Route component={NotFound} />
-    </Switch>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,39 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "@material-ui/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { MuiPickersUtilsProvider } from "@material-ui/pickers";
-import DateFnsUtils from "@date-io/date-fns";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import theme from "../src/theme";
-
-let render = () => {
-  ReactDOM.render(
-    <BrowserRouter>
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <App />
-        </ThemeProvider>
-      </MuiPickersUtilsProvider>
-    </BrowserRouter>,
-    document.getElementById("root")
-  );
-};
-
-// adding this hot module allows changes to be seen immediately on the local
-// host without the page having to be refreshed entirely for changes to be seen
-if (module.hot) {
-  module.hot.accept("./App", () => {
-    setTimeout(render);
-  });
-}
-
-render();
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
