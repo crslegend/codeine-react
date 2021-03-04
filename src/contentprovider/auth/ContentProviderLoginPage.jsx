41526929cd60d9867a02bd99921015a0cee74e04
crslegend <risheng17@gmail.com>
2021-03-04 15:17:07 +0800

    direct user to user page upon check user type

--- src/contentprovider/auth/ContentProviderLoginPage.jsx
+++ src/contentprovider/auth/ContentProviderLoginPage.jsx
@@ -94,16 +94,19 @@ const ContentProviderLoginPage = () => {
         if (res.data.user.partner) {
           Service.storeCredentials(res.data);
           history.push("/partner/home/dashboard");
-        } else {
-          setLoading(false);
-          setSbOpen(true);
-          setSnackbar({
-            ...snackbar,
-            message:
-              "The email address entered is not a registered partner. Please register first!",
-            severity: "error",
-          });
-          return;
+        } else if (res.data.user.member) {
+          Service.storeCredentials(res.data);
+          history.push("/courses");
+
+          // setLoading(false);
+          // setSbOpen(true);
+          // setSnackbar({
+          //   ...snackbar,
+          //   message:
+          //     "The email address entered is not a registered partner. Please register first!",
+          //   severity: "error",
+          // });
+          // return;
         }
       })
       .catch((err) => {
