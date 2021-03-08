1c4d7ae616ac5b57d4c8f382c8f775f21dc2be3c
crslegend <risheng17@gmail.com>
2021-03-09 00:21:03 +0800

    refactor time interval func

--- src/admin/ContentQuality/course/components/CommentsSection.jsx
+++ src/admin/ContentQuality/course/components/CommentsSection.jsx
@@ -24,6 +24,7 @@ import Toast from "../../../../components/Toast.js";
 import jwt_decode from "jwt-decode";
 import Cookies from "js-cookie";
 import NestedComments from "./NestedComments";
+import { calculateDateInterval } from "../../../../utils.js";
 
 const styles = makeStyles((theme) => ({
   dialogButtons: {
@@ -89,43 +90,6 @@ const CommentsSection = ({ materialId, user }) => {
     getCourseMaterialComments();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [materialId, pageNum]);
-
-  const calculateDateInterval = (timestamp) => {
-    const dateBefore = new Date(timestamp);
-    const dateNow = new Date();
-
-    let seconds = Math.floor((dateNow - dateBefore) / 1000);
-    let minutes = Math.floor(seconds / 60);
-    let hours = Math.floor(minutes / 60);
-    let days = Math.floor(hours / 24);
-
-    hours = hours - days * 24;
-    minutes = minutes - days * 24 * 60 - hours * 60;
-    seconds = seconds - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60;
-
-    if (days === 0) {
-      if (hours === 0) {
-        if (minutes === 0) {
-          return `${seconds} seconds ago`;
-        }
-
-        if (minutes === 1) {
-          return `${minutes} minute ago`;
-        }
-        return `${minutes} minutes ago`;
-      }
-
-      if (hours === 1) {
-        return `${hours} hour ago`;
-      }
-      return `${hours} hours ago`;
-    }
-
-    if (days === 1) {
-      return `${days} day ago`;
-    }
-    return `${days} days ago`;
-  };
 
   const handleAddComment = (id) => {
     if (commentDialogValue === "") {
