dc14905376f13c93b7399752fa5f4d7266e336b3
crslegend <risheng17@gmail.com>
2021-03-18 23:03:36 +0800

    display individual course card analytics

--- src/contentprovider/dashboard/DashboardPage.jsx
+++ src/contentprovider/dashboard/DashboardPage.jsx
@@ -3,6 +3,9 @@ import { makeStyles } from "@material-ui/core/styles";
 import PageTitle from "../../components/PageTitle";
 import Service from "../../AxiosService";
 import {
+  Card,
+  CardActionArea,
+  CardContent,
   Grid,
   IconButton,
   Paper,
@@ -10,6 +13,7 @@ import {
   Typography,
 } from "@material-ui/core";
 import { Info } from "@material-ui/icons";
+import { useHistory } from "react-router";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -19,14 +23,31 @@ const useStyles = makeStyles((theme) => ({
     marginRight: "auto",
     display: "flex",
     justifyContent: "space-between",
+    marginBottom: "30px",
   },
   numbers: {
     color: theme.palette.primary.main,
+  },
+  cardRoot: {
+    width: "300px",
+    padding: "10px 10px",
+    marginTop: "30px",
+    marginRight: "50px",
+    border: "1px solid",
+    borderRadius: 0,
+    height: "100%",
+  },
+  individualStats: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    // marginBottom: "5px",
   },
 }));
 
 const DashboardPage = () => {
   const classes = useStyles();
+  const history = useHistory();
 
   const [courses, setCourses] = useState([]);
   const [overallData, setOverallData] = useState();
@@ -96,7 +117,9 @@ const DashboardPage = () => {
           </div>
           <Typography variant="h1" className={classes.numbers}>
             {overallData &&
-              (overallData.overall_conversion_rate < 1 ? (
+              (overallData.overall_conversion_rate === 0 ? (
+                <span style={{ color: "#C74343" }}>{"0%"}</span>
+              ) : overallData.overall_conversion_rate < 1 ? (
                 <span style={{ color: "#C74343" }}>{"< 1%"}</span>
               ) : (
                 overallData.overall_conversion_rate.toFixed(4) + "%"
@@ -123,7 +146,85 @@ const DashboardPage = () => {
           </Typography>
         </div>
       </Paper>
-      <Grid container></Grid>
+      <Grid container>
+        {courses &&
+          courses.length > 0 &&
+          courses.map((course, index) => {
+            return (
+              <Grid item xs={4} key={index}>
+                <Card elevation={0} className={classes.cardRoot}>
+                  <CardActionArea
+                    style={{ height: "100%" }}
+                    onClick={() =>
+                      history.push(
+                        `/partner/home/dashboard/${course.course_id}`
+                      )
+                    }
+                  >
+                    <CardContent
+                      style={{
+                        height: "inherit",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <Typography
+                        style={{
+                          fontWeight: 600,
+                        }}
+                        variant="h6"
+                      >
+                        {course.title}
+                      </Typography>
+                      <div>
+                        <div className={classes.individualStats}>
+                          <Typography
+                            variant="body2"
+                            style={{ paddingRight: "10px" }}
+                          >
+                            Number of Views
+                          </Typography>
+                          <Typography variant="h4" className={classes.numbers}>
+                            {course.view_count}
+                          </Typography>
+                        </div>
+                        <div className={classes.individualStats}>
+                          <Typography
+                            variant="body2"
+                            style={{ paddingRight: "10px" }}
+                          >
+                            Number of Enrollments
+                          </Typography>
+                          <Typography variant="h4" className={classes.numbers}>
+                            {course.enrollment_count}
+                          </Typography>
+                        </div>
+                        <div className={classes.individualStats}>
+                          <Typography
+                            variant="body2"
+                            style={{ paddingRight: "10px" }}
+                          >
+                            Conversion Rate
+                          </Typography>
+                          <Typography variant="h4" className={classes.numbers}>
+                            {course.conversion_rate === 0 ? (
+                              <span style={{ color: "#C74343" }}>{"0%"}</span>
+                            ) : course.conversion_rate < 1 ? (
+                              <span style={{ color: "#C74343" }}>{"< 1%"}</span>
+                            ) : (
+                              course.conversion_rate.toFixed(4) + "%"
+                            )}
+                          </Typography>
+                        </div>
+                      </div>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            );
+          })}
+      </Grid>
     </div>
   );
 };
