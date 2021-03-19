84b99b90d228384d750223f3ca84000dc1e7344a
crslegend <risheng17@gmail.com>
2021-03-19 22:59:08 +0800

    display course material time taken in graph

--- src/contentprovider/dashboard/components/CourseMaterialAnalysis.jsx
+++ src/contentprovider/dashboard/components/CourseMaterialAnalysis.jsx
@@ -8,6 +8,17 @@ import {
   Select,
   Typography,
 } from "@material-ui/core";
+import {
+  Label,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+import DefaultTooltipContent from "recharts/lib/component/DefaultTooltipContent";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,12 +28,13 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
   },
   paper: {
-    padding: theme.spacing(1),
-    width: "300px",
+    padding: theme.spacing(2),
+    minWidth: "300px",
     display: "flex",
     // marginBottom: "30px",
     height: "40px",
     justifyContent: "center",
+    alignItems: "center",
   },
   numbers: {
     color: theme.palette.primary.main,
@@ -57,13 +69,70 @@ const useStyles = makeStyles((theme) => ({
       marginLeft: "0px",
     },
   },
+  tooltip: {
+    backgroundColor: "#fff",
+    border: "1px solid #ddd",
+    padding: theme.spacing(1),
+  },
 }));
 
 const CourseMaterialAnalysis = ({ timeTakenCourseMaterial }) => {
   const classes = useStyles();
   console.log(timeTakenCourseMaterial);
 
+  const handleSelectChapter = (e) => {
+    setSelectedChapterId(e.target.value);
+
+    let index;
+    for (let i = 0; i < timeTakenCourseMaterial.length; i++) {
+      if (timeTakenCourseMaterial[i].chapter_id === e.target.value) {
+        index = i;
+        break;
+      }
+    }
+    const courseMaterials = timeTakenCourseMaterial[index].course_materials;
+    // console.log(courseMaterials);
+    let loadedData = [];
+    let obj = {};
+    for (let i = 0; i < courseMaterials.length; i++) {
+      obj = {
+        name: courseMaterials[i].course_material_title,
+        Type:
+          courseMaterials[i].material_type
+            .toLowerCase()
+            .charAt(0)
+            .toUpperCase() +
+          courseMaterials[i].material_type.toLowerCase().slice(1),
+        Time: parseFloat(
+          (courseMaterials[i].average_time_taken / 3600).toFixed(6)
+        ),
+      };
+      loadedData.push(obj);
+    }
+    setData(loadedData);
+  };
+
+  const [data, setData] = useState([]);
   const [selectedChapterId, setSelectedChapterId] = useState("");
+
+  const CustomTooltip = (props) => {
+    const { active, payload, label } = props;
+    // console.log(props);
+    if (active && payload && payload.length) {
+      return (
+        <div className={classes.tooltip}>
+          {`${payload[0].payload.name}`}
+          <br />
+          {`${payload[0].payload.Type}`}
+          <br />
+          <span
+            style={{ color: "#437FC7" }}
+          >{`${payload[0].payload.Time} hrs`}</span>
+        </div>
+      );
+    }
+    return null;
+  };
 
   return (
     <div className={classes.root}>
@@ -76,7 +145,7 @@ const CourseMaterialAnalysis = ({ timeTakenCourseMaterial }) => {
               variant="outlined"
               value={selectedChapterId ? selectedChapterId : ""}
               onChange={(e) => {
-                setSelectedChapterId(e.target.value);
+                handleSelectChapter(e);
               }}
             >
               {timeTakenCourseMaterial &&
@@ -94,9 +163,7 @@ const CourseMaterialAnalysis = ({ timeTakenCourseMaterial }) => {
         <div style={{ marginLeft: "10px" }}>
           <Paper className={classes.paper}>
             {selectedChapterId === "" ? (
-              <div>
-                <Typography variant="h6">No Chapter Selected</Typography>
-              </div>
+              <Typography variant="body2">No Chapter Selected</Typography>
             ) : (
               <div>
                 {timeTakenCourseMaterial &&
@@ -104,17 +171,16 @@ const CourseMaterialAnalysis = ({ timeTakenCourseMaterial }) => {
                   timeTakenCourseMaterial.map((chapter, index) => {
                     if (chapter.chapter_id === selectedChapterId) {
                       return (
-                        <div>
-                          <Typography
-                            variant="h6"
-                            style={{ textAlign: "center" }}
-                          >
-                            <span style={{ fontWeight: 600 }}>
-                              Chapter {index + 1}:{" "}
-                            </span>
-                            {chapter.chapter_title}
-                          </Typography>
-                        </div>
+                        <Typography
+                          key={index}
+                          variant="body2"
+                          style={{ textAlign: "center" }}
+                        >
+                          <span style={{ fontWeight: 600 }}>
+                            Chapter {index + 1}:{" "}
+                          </span>
+                          {chapter.chapter_title}
+                        </Typography>
                       );
                     }
                     return null;
@@ -124,6 +190,47 @@ const CourseMaterialAnalysis = ({ timeTakenCourseMaterial }) => {
           </Paper>
         </div>
       </div>
+
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart
+          width={600}
+          height={400}
+          data={data}
+          margin={{
+            top: 15,
+            right: 30,
+            left: 40,
+            bottom: 25,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name">
+            <Label
+              value="Course Materials in Chapter"
+              position="bottom"
+              offset={10}
+              style={{ textAnchor: "middle" }}
+            />
+          </XAxis>
+          <YAxis>
+            <Label
+              value="Average Time Taken (Hours)"
+              position="left"
+              angle={-90}
+              offset={30}
+              style={{ textAnchor: "middle" }}
+            />
+          </YAxis>
+          <Tooltip content={<CustomTooltip />} />
+          <Line
+            type="monotone"
+            dataKey="Time"
+            stroke="#437FC7"
+            strokeWidth={2}
+            activeDot={{ r: 7 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 };
