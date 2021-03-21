9360253fb55e2f0ded865b24f3ce6a6284301c39
crslegend <risheng17@gmail.com>
2021-03-21 12:46:46 +0800

    added admin earning report

--- src/admin/Analytics/AnalyticsPage.jsx
+++ src/admin/Analytics/AnalyticsPage.jsx
@@ -1,22 +1,351 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Typography } from "@material-ui/core";
+import {
+  FormControl,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  Typography,
+} from "@material-ui/core";
+import TooltipMui from "@material-ui/core/Tooltip";
+import { Add, DragHandle, Info } from "@material-ui/icons";
+import Service from "../../AxiosService";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    minHeight: "100vh",
+    minHeight: "100%",
     display: "flex",
     flexDirection: "column",
     fontDisplay: "swap",
+  },
+  numbers: {
+    color: theme.palette.primary.main,
+  },
+  paper: {
+    padding: theme.spacing(4),
+    width: "80%",
+    marginLeft: "auto",
+    marginRight: "auto",
+    display: "flex",
+    flexDirection: "column",
+    // justifyContent: "space-between",
+    marginBottom: "30px",
+  },
+  formControl: {
+    marginTop: "15px",
+    marginBottom: "10px",
+    width: "200px",
   },
 }));
 
 const AdminAnalyticsPage = () => {
   const classes = useStyles();
 
+  const [numDays, setNumDays] = useState();
+  const [earningsReport, setEarningsReport] = useState();
+
+  const getAnalytics = async () => {
+    if (numDays && numDays !== "") {
+      Service.client
+        .get(`/analytics/first-enrollment-count`, { params: { days: numDays } })
+        .then((res) => {
+          // console.log(res);
+        })
+        .catch((err) => console.log(err));
+
+      Service.client
+        .get(`/analytics/course-enrollment-count`, {
+          params: { days: numDays },
+        })
+        .then((res) => {
+          // console.log(res);
+        })
+        .catch((err) => console.log(err));
+
+      Service.client
+        .get(`/analytics/admin-earnings-report`, { params: { days: numDays } })
+        .then((res) => {
+          // console.log(res);
+          let obj = {
+            expenses: res.data.expenses,
+            total_contribution_income:
+              res.data.total_contribution_income === null
+                ? 0
+                : res.data.total_contribution_income,
+            total_subscription_revenue: res.data.total_subscription_revenue,
+            total_income:
+              res.data.total_contribution_income === null
+                ? 0 + res.data.total_subscription_revenue
+                : res.data.total_contribution_income +
+                  res.data.total_subscription_revenue,
+          };
+          obj = {
+            ...obj,
+            total_income_less_expenses: obj.total_income - obj.expenses,
+          };
+          setEarningsReport(obj);
+        })
+        .catch((err) => console.log(err));
+
+      Service.client
+        .get(`/analytics/platform-health-check`, { params: { days: numDays } })
+        .then((res) => {
+          // console.log(res);
+        })
+        .catch((err) => console.log(err));
+    } else {
+      Service.client
+        .get(`/analytics/first-enrollment-count`)
+        .then((res) => {
+          // console.log(res);
+        })
+        .catch((err) => console.log(err));
+
+      Service.client
+        .get(`/analytics/course-enrollment-count`)
+        .then((res) => {
+          // console.log(res);
+        })
+        .catch((err) => console.log(err));
+
+      Service.client
+        .get(`/analytics/admin-earnings-report`)
+        .then((res) => {
+          console.log(res);
+          let obj = {
+            expenses: res.data.expenses,
+            total_contribution_income:
+              res.data.total_contribution_income === null
+                ? 0
+                : res.data.total_contribution_income,
+            total_subscription_revenue: res.data.total_subscription_revenue,
+            total_income:
+              res.data.total_contribution_income === null
+                ? 0 + res.data.total_subscription_revenue
+                : res.data.total_contribution_income +
+                  res.data.total_subscription_revenue,
+          };
+          obj = {
+            ...obj,
+            total_income_less_expenses: obj.total_income - obj.expenses,
+          };
+          setEarningsReport(obj);
+        })
+        .catch((err) => console.log(err));
+
+      Service.client
+        .get(`/analytics/platform-health-check`)
+        .then((res) => {
+          // console.log(res);
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
+  useEffect(() => {
+    getAnalytics();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    getAnalytics();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [numDays]);
+
   return (
     <div className={classes.root}>
-      <Typography>AdminAnalyticsPage</Typography>
+      <Typography variant="h2" style={{ paddingBottom: "20px" }}>
+        Analytics
+      </Typography>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h6" style={{ paddingRight: "15px" }}>
+          View By
+        </Typography>
+        <FormControl
+          margin="dense"
+          variant="outlined"
+          className={classes.formControl}
+        >
+          <InputLabel>Date Range</InputLabel>
+          <Select
+            label="Date Range"
+            value={numDays ? numDays : ""}
+            onChange={(e) => {
+              setNumDays(e.target.value);
+            }}
+            style={{ backgroundColor: "#fff" }}
+          >
+            <MenuItem value="">
+              <em>Select a date range</em>
+            </MenuItem>
+            <MenuItem value="7">Past Week</MenuItem>
+            <MenuItem value="14">Past 2 Weeks</MenuItem>
+            <MenuItem value="30">Past Month</MenuItem>
+            <MenuItem value="90">Past 3 Months</MenuItem>
+            <MenuItem value="240">Past 6 Months</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <Paper className={classes.paper}>
+        <Typography
+          variant="h6"
+          style={{ fontWeight: 600, paddingBottom: "10px" }}
+        >
+          Earnings Report
+        </Typography>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+            marginBottom: "20px",
+          }}
+        >
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <div style={{ display: "flex" }}>
+              <Typography variant="h6">Fundings</Typography>
+              <TooltipMui
+                title={
+                  <Typography variant="body2">
+                    Total amount of funding received from organizations
+                  </Typography>
+                }
+              >
+                <IconButton disableRipple size="small">
+                  <Info fontSize="small" color="primary" />
+                </IconButton>
+              </TooltipMui>
+            </div>
+            <Typography variant="h1" className={classes.numbers}>
+              {earningsReport &&
+                (earningsReport.total_contribution_income === 0 ||
+                !earningsReport.total_contribution_income ? (
+                  <span style={{ color: "#C74343" }}>{"$0"}</span>
+                ) : (
+                  "$" + earningsReport.total_contribution_income
+                ))}
+            </Typography>
+          </div>
+          <Add style={{ fontSize: "50px" }} color="disabled" />
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <div style={{ display: "flex" }}>
+              <Typography variant="h6">Subscriptions</Typography>
+              <TooltipMui
+                title={
+                  <Typography variant="body2">
+                    Total amount of earnings from pro-tier members
+                  </Typography>
+                }
+              >
+                <IconButton disableRipple size="small">
+                  <Info fontSize="small" color="primary" />
+                </IconButton>
+              </TooltipMui>
+            </div>
+            <Typography variant="h1" className={classes.numbers}>
+              {earningsReport &&
+                (earningsReport.total_subscription_revenue === 0 ||
+                !earningsReport.total_subscription_revenue ? (
+                  <span style={{ color: "#C74343" }}>{"$0"}</span>
+                ) : (
+                  "$" + earningsReport.total_subscription_revenue
+                ))}
+            </Typography>
+          </div>
+          <DragHandle style={{ fontSize: "50px" }} color="disabled" />
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <div style={{ display: "flex" }}>
+              <Typography variant="h6">Total Income</Typography>
+              <TooltipMui
+                title={
+                  <Typography variant="body2">
+                    Total income before accounting for expenses
+                  </Typography>
+                }
+              >
+                <IconButton disableRipple size="small">
+                  <Info fontSize="small" color="primary" />
+                </IconButton>
+              </TooltipMui>
+            </div>
+            <Typography variant="h1" className={classes.numbers}>
+              {earningsReport &&
+                (earningsReport.total_income === 0 ? (
+                  <span style={{ color: "#C74343" }}>{"$0"}</span>
+                ) : (
+                  "$" + earningsReport.total_income
+                ))}
+            </Typography>
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+          }}
+        >
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <div style={{ display: "flex" }}>
+              <Typography variant="h6">Expenses</Typography>
+              <TooltipMui
+                title={
+                  <Typography variant="body2">
+                    Total expenses needed to run Codeine
+                  </Typography>
+                }
+              >
+                <IconButton disableRipple size="small">
+                  <Info fontSize="small" color="primary" />
+                </IconButton>
+              </TooltipMui>
+            </div>
+            <Typography variant="h1" className={classes.numbers}>
+              {earningsReport && (
+                <span style={{ color: "#C74343" }}>
+                  {`$${earningsReport.expenses}`}
+                </span>
+              )}
+            </Typography>
+          </div>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <div style={{ display: "flex" }}>
+              <Typography variant="h6">Net Income</Typography>
+              <TooltipMui
+                title={
+                  <Typography variant="body2">
+                    Total net income after accounting for expenses
+                  </Typography>
+                }
+              >
+                <IconButton disableRipple size="small">
+                  <Info fontSize="small" color="primary" />
+                </IconButton>
+              </TooltipMui>
+            </div>
+            <Typography variant="h1" className={classes.numbers}>
+              {earningsReport &&
+                (earningsReport.total_income_less_expenses < 0 ? (
+                  <span style={{ color: "#C74343" }}>{`-$${
+                    earningsReport.total_income_less_expenses * -1
+                  }`}</span>
+                ) : earningsReport.total_income_less_expenses === 0 ? (
+                  <span style={{ color: "#C74343" }}>{`$0`}</span>
+                ) : (
+                  "$" + earningsReport.total_income_less_expenses
+                ))}
+            </Typography>
+          </div>
+        </div>
+      </Paper>
+      <Paper className={classes.paper}></Paper>
     </div>
   );
 };
