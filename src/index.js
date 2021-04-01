f1d5e7c3b1f9f439733f0d2c451e36ab2d6fc3c9
Tze Ming <e0311042@u.nus.edu>
2021-04-01 18:14:06 +0800

    fix typo

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import DateFnsUtils from "@date-io/date-fns";
 import theme from "../src/theme";
 
 import { Provider } from "react-redux";
-import store from "./redux/Store.jss";
+import store from "./redux/Store.jsx";
 
 let render = () => {
   ReactDOM.render(
