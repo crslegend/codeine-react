1c0c13bcc1955892a6f3b133990befb1f235d05b
eliz <e0319168@u.nus.edu>
2021-04-05 18:02:46 +0800

    minor fix

--- src/member/MemberNavBar.jsx
+++ src/member/MemberNavBar.jsx
@@ -19,7 +19,6 @@ import { Dashboard, Timeline, Work } from "@material-ui/icons";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import InsertDriveFileIcon from "@material-ui/icons/InsertDriveFile";
 import HelpOutlineOutlinedIcon from "@material-ui/icons/HelpOutlineOutlined";
-import Work from "@material-ui/icons/Work";
 import AccountBalanceWalletIcon from "@material-ui/icons/AccountBalanceWallet";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faNewspaper } from "@fortawesome/free-solid-svg-icons";
