4359bae5a652ef5bb3bcf489d3ad530bef7cf9d7
Tze Ming <e0311042@u.nus.edu>
2021-04-06 16:13:49 +0800

    style touch up view all code reviews

--- src/codeReview/ViewAllCodeReviews.jsx
+++ src/codeReview/ViewAllCodeReviews.jsx
@@ -9,11 +9,13 @@ import jwt_decode from "jwt-decode";
 import components from "./components/NavbarComponents";
 import { Avatar, Button, Chip, Paper, Typography } from "@material-ui/core";
 import LinkMui from "@material-ui/core/Link";
-import { Add, Favorite, Forum, People, Person } from "@material-ui/icons";
+import { Add, Favorite, Forum, Grade, People, Person } from "@material-ui/icons";
 import Toast from "../components/Toast.js";
 import AddSnippetDialog from "./components/AddSnippetDialog";
 
-const styles = makeStyles((theme) => ({
+import hljs from "highlight.js";
+
+const useStyles = makeStyles((theme) => ({
   root: {
     minHeight: "100vh",
     display: "flex",
@@ -40,13 +42,25 @@ const styles = makeStyles((theme) => ({
     fontWeight: 600,
     fontFamily: "Roboto Mono",
   },
+  paperRoot: {
+    display: "flex",
+    marginBottom: theme.spacing(2),
+  },
+  codePreview: {
+    width: "25%",
+    height: "200px",
+    padding: theme.spacing(1),
+    opacity: 0.7,
+    background: "rgba(164, 201, 245, 0.2)",
+  },
   codeReview: {
-    marginBottom: "10px",
-    padding: theme.spacing(4),
+    flexGrow: 1,
+    padding: theme.spacing(3),
     display: "flex",
     flexDirection: "column",
   },
   linkMui: {
+    margin: theme.spacing(1),
     fontSize: 28,
     cursor: "pointer",
     "&:hover": {
@@ -61,10 +75,24 @@ const styles = makeStyles((theme) => ({
       color: "#065cc4",
     },
   },
+  chip: {
+    fontSize: "12px",
+    height: 28,
+    margin: theme.spacing(1, 0),
+  },
+  codeBody: {
+    flexGrow: 1,
+    padding: theme.spacing(0.8, 2),
+    display: "flex",
+    overflow: "hidden",
+  },
+  likesContainer: {
+    color: theme.palette.yellow.main,
+  },
 }));
 
 const ViewAllCodeReviews = () => {
-  const classes = styles();
+  const classes = useStyles();
   const history = useHistory();
 
   const [sbOpen, setSbOpen] = useState(false);
@@ -98,6 +126,8 @@ const ViewAllCodeReviews = () => {
 
   const [viewMySnippet, setViewMySnippet] = useState(false);
   const [codeReviews, setCodeReviews] = useState();
+
+  console.log(codeReviews);
 
   const checkIfLoggedIn = () => {
     if (Cookies.get("t1")) {
@@ -153,9 +183,8 @@ const ViewAllCodeReviews = () => {
       <Chip
         key={index}
         label={label}
+        className={classes.chip}
         style={{
-          marginRight: "10px",
-          marginBottom: "10px",
           color: fontColor ? fontColor : "#000",
           fontWeight: 600,
           backgroundColor: backgroundColor,
@@ -277,7 +306,7 @@ const ViewAllCodeReviews = () => {
       <div className={classes.content}>
         <div className={classes.title}>
           <Typography variant="h2" className={classes.heading}>
-            code review
+            code reviews
           </Typography>
           <div>
             {loggedIn &&
@@ -331,81 +360,100 @@ const ViewAllCodeReviews = () => {
           {codeReviews && codeReviews.length > 0 ? (
             codeReviews.map((code, index) => {
               return (
-                <Paper key={index} className={classes.codeReview}>
-                  <div>
+                <Paper key={index} className={classes.paperRoot}>
+                  <div className={classes.codePreview}>
+                    {code &&
+                      code.code
+                        .split("\n")
+                        .slice(0, 6)
+                        .map((line) => (
+                          <div className={classes.codeBody}>
+                            <pre style={{ margin: 0 }}>
+                              <div
+                                dangerouslySetInnerHTML={{
+                                  __html: hljs.highlightAuto(line, [code.coding_languages[0]]).value,
+                                }}
+                              />
+                            </pre>
+                          </div>
+                        ))}
+                  </div>
+                  <div className={classes.codeReview}>
                     <LinkMui className={classes.linkMui} onClick={() => history.push(`/codereview/${code.id}`)}>
                       {code.title}
                     </LinkMui>
-                  </div>
-                  <div style={{ marginTop: "20px" }}>
-                    {code &&
-                      code.categories.length > 0 &&
-                      code.categories.map((category, index) => {
-                        if (category === "FE") {
-                          return resuableChip("Frontend", index, "#DD8B8B");
-                        } else if (category === "BE") {
-                          return resuableChip("Backend", index, "#A0DD8B");
-                        } else if (category === "DB") {
-                          return resuableChip("Database Administration", index, "#8B95DD");
-                        } else if (category === "SEC") {
-                          return resuableChip("Security", index, "#DDB28B");
-                        } else if (category === "UI") {
-                          return resuableChip("UI/UX", index, "#DDD58B");
-                        } else if (category === "ML") {
-                          return resuableChip("Machine Learning", index, "#8BD8DD");
-                        } else {
-                          return null;
-                        }
-                      })}
-                    {code &&
-                      code.coding_languages.length > 0 &&
-                      code.coding_languages.map((language, index) => {
-                        if (language === "PY") {
-                          return resuableChip("Python", index, "#3675A9", "#fff");
-                        } else if (language === "JAVA") {
-                          return resuableChip("Java", index, "#E57001", "#fff");
-                        } else if (language === "JS") {
-                          return resuableChip("Javascript", index, "#F7DF1E");
-                        } else if (language === "RUBY") {
-                          return resuableChip("Ruby", index, "#CC0000");
-                        } else if (language === "CPP") {
-                          return resuableChip("C++", index, "#004482", "#fff");
-                        } else if (language === "CS") {
-                          return resuableChip("C#", index, "#6A1577", "#fff");
-                        } else if (language === "HTML") {
-                          return resuableChip("HTML", index, "#E44D26", "#fff");
-                        } else if (language === "CSS") {
-                          return resuableChip("CSS", index, "#264DE4", "#fff");
-                        } else {
-                          return null;
-                        }
-                      })}
-                  </div>
-                  <div style={{ display: "flex", alignItems: "center" }}>
-                    <div style={{ display: "flex", alignItems: "center" }}>
-                      <Favorite style={{ marginRight: "5px" }} />
-                      {`${code && code.likes}`}
+                    <div>
+                      {code &&
+                        code.categories.length > 0 &&
+                        code.categories.map((category, index) => {
+                          if (category === "FE") {
+                            return resuableChip("Frontend", index, "#DD8B8B");
+                          } else if (category === "BE") {
+                            return resuableChip("Backend", index, "#A0DD8B");
+                          } else if (category === "DB") {
+                            return resuableChip("Database Administration", index, "#8B95DD");
+                          } else if (category === "SEC") {
+                            return resuableChip("Security", index, "#DDB28B");
+                          } else if (category === "UI") {
+                            return resuableChip("UI/UX", index, "#DDD58B");
+                          } else if (category === "ML") {
+                            return resuableChip("Machine Learning", index, "#8BD8DD");
+                          } else {
+                            return null;
+                          }
+                        })}
+                      {code &&
+                        code.coding_languages.length > 0 &&
+                        code.coding_languages.map((language, index) => {
+                          if (language === "PY") {
+                            return resuableChip("Python", index, "#3675A9", "#fff");
+                          } else if (language === "JAVA") {
+                            return resuableChip("Java", index, "#E57001", "#fff");
+                          } else if (language === "JS") {
+                            return resuableChip("Javascript", index, "#F7DF1E");
+                          } else if (language === "RUBY") {
+                            return resuableChip("Ruby", index, "#CC0000");
+                          } else if (language === "CPP") {
+                            return resuableChip("C++", index, "#004482", "#fff");
+                          } else if (language === "CS") {
+                            return resuableChip("C#", index, "#6A1577", "#fff");
+                          } else if (language === "HTML") {
+                            return resuableChip("HTML", index, "#E44D26", "#fff");
+                          } else if (language === "CSS") {
+                            return resuableChip("CSS", index, "#264DE4", "#fff");
+                          } else {
+                            return null;
+                          }
+                        })}
                     </div>
-                    <div
-                      style={{
-                        marginLeft: "auto",
-                        display: "flex",
-                      }}
-                    >
-                      <div>
-                        {code.user.profile_photo && code.user.profile_photo ? (
-                          <Avatar style={{ marginRight: "15px" }} src={code.user && code.user.profile_photo} />
-                        ) : (
-                          <Avatar style={{ marginRight: "15px" }}>{code.user && code.user.first_name.charAt(0)}</Avatar>
-                        )}
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <div style={{ display: "flex", alignItems: "center" }}>
+                        <Grade style={{ marginRight: "8px" }} className={classes.likesContainer} />
+                        Likes: {code && code.likes}
                       </div>
-                      <div style={{ flexDirection: "column" }}>
-                        <LinkMui className={classes.linkMui1}>
-                          {`${code && code.user.first_name} ${code && code.user.last_name}`}
-                        </LinkMui>
-                        <Typography variant="body2" style={{ opacity: 0.8 }}>
-                          {` asked ${code && calculateDateInterval(code.timestamp)}`}
-                        </Typography>
+                      <div
+                        style={{
+                          marginLeft: "auto",
+                          display: "flex",
+                        }}
+                      >
+                        <div>
+                          {code.user.profile_photo && code.user.profile_photo ? (
+                            <Avatar style={{ marginRight: "15px" }} src={code.user && code.user.profile_photo} />
+                          ) : (
+                            <Avatar style={{ marginRight: "15px" }}>
+                              {code.user && code.user.first_name.charAt(0)}
+                            </Avatar>
+                          )}
+                        </div>
+                        <div style={{ flexDirection: "column" }}>
+                          <LinkMui className={classes.linkMui1}>
+                            {`${code && code.user.first_name} ${code && code.user.last_name}`}
+                          </LinkMui>
+                          <Typography variant="body2" style={{ opacity: 0.8 }}>
+                            {` submitted ${code && calculateDateInterval(code.timestamp)}`}
+                          </Typography>
+                        </div>
                       </div>
                     </div>
                   </div>
