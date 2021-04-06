098bc7b910eaab379b9959c81c4bdd3243b3bc76
Tze Ming <e0311042@u.nus.edu>
2021-04-06 16:21:51 +0800

    resolve lint errors

--- src/codeReview/components/CodeLine.jsx
+++ src/codeReview/components/CodeLine.jsx
@@ -1,7 +1,7 @@
 import React, { useState, Fragment } from "react";
 import { useParams } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-import { Button, Card, CardActions, CardContent, IconButton } from "@material-ui/core";
+import { IconButton } from "@material-ui/core";
 import { AddCircle } from "@material-ui/icons";
 import hljs from "highlight.js";
 
